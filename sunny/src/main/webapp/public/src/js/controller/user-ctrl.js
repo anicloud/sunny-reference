b373ed5a01c4647df47a534aa5ac809c7b9c1092
zhangzhaoyu <zhangzhaoyu0524@163.com>
2015-10-02 23:21:34 +0800

    add

--- sunny/src/main/webapp/public/src/js/controller/user-ctrl.js
+++ sunny/src/main/webapp/public/src/js/controller/user-ctrl.js
@@ -7,13 +7,23 @@ anicloud.sunny = anicloud.sunny || {};
 anicloud.sunny.controller = anicloud.sunny.controller || {};
 
 anicloud.sunny.controller.UserCtrl = function ($rootScope, $scope, $window, UserService) {
+    var user = $.cookie('sunny_user');
+    if (user != undefined) {
+        var usercookie = JSON.parse(user);
+        $scope.currentUser = usercookie;
+    }
+
     $scope.logout = function () {
         UserService.logout(function(data) {
             if (data.status == "success") {
-                $window.location.href = "/sunny";
+                $window.location.href = "logout";
             } else {
                 console.log("user logout error.");
             }
         });
+    }
+
+    $scope.switchUser = function() {
+        $window.location.href = "switchUser?hashUserId=" + $scope.currentUser.hashUserId;
     }
 }
