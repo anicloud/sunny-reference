82c1e4442cfb5df899fe4ae532bc68693054428d
Mr.Programmer <m_programmer@163.com>
2016-03-15 17:47:57 +0800

    update test

--- sunny/src/test/java/com/anicloud/sunny/application/json/JacksonTest.java
+++ sunny/src/test/java/com/anicloud/sunny/application/json/JacksonTest.java
@@ -29,7 +29,7 @@ import java.util.List;
  */
 public class JacksonTest {
 
-    private String filePath = "classpath:properties/StubMeta.json";
+    private String filePath = "classpath:properties/StubIdentity.json";
 
 
     @Test
