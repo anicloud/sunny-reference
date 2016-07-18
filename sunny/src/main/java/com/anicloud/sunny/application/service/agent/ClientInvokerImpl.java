aa17a1c94bc5e11119bc5778b46ac2f2340b06a1
LiHui <812486664@qq.com>
2016-07-18 15:35:22 +0800

    ClientInvoker

--- sunny/src/main/java/com/anicloud/sunny/application/service/agent/ClientInvokerImpl.java
+++ sunny/src/main/java/com/anicloud/sunny/application/service/agent/ClientInvokerImpl.java
@@ -1,12 +1,15 @@
 package com.anicloud.sunny.application.service.agent;
 
+import com.ani.agent.service.core.validate.DomainObjectValidator;
 import com.ani.agent.service.service.websocket.ClientInvokable;
 import com.ani.bus.service.commons.dto.anistub.AniStub;
 import com.ani.bus.service.commons.dto.anistub.Argument;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
+import javax.validation.ValidationException;
 import javax.websocket.CloseReason;
 import javax.websocket.EncodeException;
 import java.io.IOException;
@@ -19,13 +22,29 @@ import java.util.List;
  */
 @Component
 public class ClientInvokerImpl implements ClientInvokable {
-
-    @Resource
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientInvokerImpl.class);
     private ClientInvokable clientInvokable;
 
+
     @Override
-    public List<Argument> invokeAniObjectSync(AniStub aniStub) throws IOException, EncodeException {
-        // TODO
+    public List<Argument> invokeAniObjectSync(AniStub stub) throws IOException, EncodeException {
+        if (!DomainObjectValidator.isDomainObjectValid(stub)) {
+            throw new ValidationException("Invalid AniStub Instance.");
+        }
+        List<Argument> input = stub.getInputValues();
+        if(input != null) {
+            LOGGER.info("\tsize:" + input.size());
+            for(int i=0; i<input.size(); i++) {
+                Argument argument = input.get(i);
+                LOGGER.info("\targument["+i+"] name:"+argument.getName());
+            }
+        }
+
+        LOGGER.info("Invocation begins.......");
+
+        LOGGER.info("Invocation ends......");
+
+
         return null;
     }
 
