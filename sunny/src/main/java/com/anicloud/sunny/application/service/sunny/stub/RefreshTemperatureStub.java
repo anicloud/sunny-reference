c35470195af4956c0eb5d456bd0d00a168d4af44
LiHui <812486664@qq.com>
2016-10-28 18:57:00 +0800

    device invoke sunny stub

--- sunny/src/main/java/com/anicloud/sunny/application/service/sunny/stub/RefreshTemperatureStub.java
+++ sunny/src/main/java/com/anicloud/sunny/application/service/sunny/stub/RefreshTemperatureStub.java
@@ -2,16 +2,29 @@ package com.anicloud.sunny.application.service.sunny.stub;
 
 import com.ani.bus.service.commons.dto.anistub.AniStub;
 import com.ani.bus.service.commons.dto.anistub.Argument;
+import com.anicloud.sunny.application.dto.device.DeviceAndUserRelationDto;
+import com.anicloud.sunny.application.service.device.DeviceAndUserRelationServcie;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
  * Created by lihui on 16-10-28.
  */
 public class RefreshTemperatureStub implements SunnyStub {
+
+    @Resource
+    private DeviceAndUserRelationServcie relationService;
+
     @Override
     public List<Argument> invokeStub(AniStub stub) {
+        Long targetObjectId = stub.getTargetObjectId();
+        Long objectId = stub.getAccountId();
+        List<Argument> inputValues = stub.getInputValues();
+//        DeviceAndUserRelationDto relation = relationService.getDeviceAndUserRelation(stub.getKeyId())
+        for (Argument arg : inputValues) {
 
+        }
         return null;
     }
 }
