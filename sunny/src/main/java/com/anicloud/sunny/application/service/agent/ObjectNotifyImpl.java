8fe44255ea8eee7287347da4911474475188f170
wyf <1336874736@qq.com>
2017-01-07 15:18:20 +0800

    update slave  state

--- sunny/src/main/java/com/anicloud/sunny/application/service/agent/ObjectNotifyImpl.java
+++ sunny/src/main/java/com/anicloud/sunny/application/service/agent/ObjectNotifyImpl.java
@@ -3,6 +3,7 @@ package com.anicloud.sunny.application.service.agent;
 import com.ani.agent.service.commons.object.enumeration.DeviceState;
 import com.ani.agent.service.service.websocket.ObjectNotify;
 import com.ani.bus.service.commons.dto.anidevice.DeviceMasterObjInfoDto;
+import com.anicloud.sunny.application.constant.Constants;
 import com.anicloud.sunny.application.dto.device.DeviceDto;
 import com.anicloud.sunny.application.service.device.DeviceService;
 import com.anicloud.sunny.application.service.init.ApplicationInitService;
@@ -11,6 +12,7 @@ import com.anicloud.sunny.infrastructure.jms.DeviceStateQueueService;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by wyf on 16-7-11.
@@ -27,20 +29,28 @@ public class ObjectNotifyImpl implements ObjectNotify{
 
     @Override
     public void deviceConectedNotify(Long objectId, String description) {
-        DeviceDto deviceDto = deviceService.getDeviceByIdentificationCode(Device.buildIdentificationCode(objectId,-1));
-        if(deviceDto != null){
-            deviceService.modifyDeviceState(deviceDto,DeviceState.CONNECTED);
-            deviceDto.deviceState = DeviceState.CONNECTED;
-            deviceStateQueueService.updateDeviceState(deviceDto);
-        }
+        updateObjectState(objectId, DeviceState.CONNECTED);
     }
 
     @Override
     public void deviceDisconnectedNotify(Long objectId, String description) {
+        updateObjectState(objectId, DeviceState.DISCONNECTED);
+    }
+
+    private void updateObjectState(Long objectId, DeviceState state) {
+        List<Integer> slaves = Constants.DEVICE_ID_RELATION_MAP.get(objectId);
+        for (Integer slaveId:slaves) {
+            DeviceDto deviceDto = deviceService.getDeviceByIdentificationCode(Device.buildIdentificationCode(objectId,slaveId));
+            if(deviceDto != null){
+                deviceService.modifyDeviceState(deviceDto,state);
+                deviceDto.deviceState = state;
+                deviceStateQueueService.updateDeviceState(deviceDto);
+            }
+        }
         DeviceDto deviceDto = deviceService.getDeviceByIdentificationCode(Device.buildIdentificationCode(objectId,-1));
         if(deviceDto != null){
-            deviceService.modifyDeviceState(deviceDto,DeviceState.DISCONNECTED);
-            deviceDto.deviceState = DeviceState.DISCONNECTED;
+            deviceService.modifyDeviceState(deviceDto,state);
+            deviceDto.deviceState = state;
             deviceStateQueueService.updateDeviceState(deviceDto);
         }
     }
