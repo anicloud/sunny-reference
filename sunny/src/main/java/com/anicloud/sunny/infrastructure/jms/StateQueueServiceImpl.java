83bcdc813a793f86fb7edf327fada5bbbe5e0730
LiHui <812486664@qq.com>
2017-01-10 10:16:07 +0800

    设备绑定、解绑、分享、取消分享状态通知

--- sunny/src/main/java/com/anicloud/sunny/infrastructure/jms/StateQueueServiceImpl.java
+++ sunny/src/main/java/com/anicloud/sunny/infrastructure/jms/StateQueueServiceImpl.java
@@ -1,14 +1,12 @@
 package com.anicloud.sunny.infrastructure.jms;
 
 import com.anicloud.sunny.application.dto.JmsTypicalMessage;
-import com.anicloud.sunny.application.dto.device.DeviceAndUserRelationDto;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.List;
 
-/**
+/** 
  * Created by lihui on 17-1-9.
  */
 @Component("stateQueueService")
